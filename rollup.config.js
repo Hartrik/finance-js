99f98e7b3eaaf31d727c791dbe0c525ca4607cc2
Patrik Harag <patrikharag@gmail.com>
2023-10-27 22:41:15 +0200

    Add minimized build

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,7 @@
 import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import { string } from "rollup-plugin-string";
+import terser from '@rollup/plugin-terser';
 import pkg from './package.json';
 
 export default [
@@ -31,6 +32,26 @@ export default [
                 include: "examples/*.*",
                 exclude: []
             })
+        ]
+    },
+
+    // browser-friendly UMD build - for public use - MINIMIZED
+    {
+        input: 'src/dist-public/main.js',
+        output: {
+            name: 'FinanceJS',
+            file: pkg.browser_public_min,
+            format: 'umd'
+        },
+        plugins: [
+            resolve(), // so Rollup can find libraries
+            commonjs(), // so Rollup can convert libraries to an ES modules
+            string({
+                include: "examples/*.*",
+                exclude: []
+            }),
+
+            terser()
         ]
     },
 
